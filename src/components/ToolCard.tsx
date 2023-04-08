b4758496d9d77e98a679b9ae1e99b153283fcbec
Rich1997 <32636319+Rich1997@users.noreply.github.com>
2023-04-09 00:21:27 +0530

    1 file modified

---
+++ src/components/ToolCard.tsx
@@ -0,0 +1,10 @@
+const ToolCard = () => {
+    return (
+        <div className="tracking-tight text-8xl w-full mx-auto p-6 max-w-6xl">
+            Pick your tool, <br />
+            <span className="font-[900]">quick!</span>
+        </div>
+    );
+};
+
+export default ToolCard;
