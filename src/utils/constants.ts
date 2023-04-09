af2a5edd053ed59f83a81d07caa35335503bac20
Rich1997 <32636319+Rich1997@users.noreply.github.com>
2023-04-09 10:01:13 +0530

    1 file deleted

--- src/utils/constants.ts
+++
@@ -1,9 +0,0 @@
-export const themes = [
-    { option: 'Light', icon: 'SunIcon' },
-    { option: 'Dark', icon: 'MoonIcon' },
-];
-
-export const fonts = [
-    { option: 'Inter', value: 'inter' },
-    { option: 'Satoshi', value: 'satoshi' },
-];
