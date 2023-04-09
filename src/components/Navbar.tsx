840fd4f44a524aa669d41df3ad2a520dbcb872ec
Rich1997 <32636319+Rich1997@users.noreply.github.com>
2023-04-09 16:22:46 +0530

    1 file modified

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,27 +6,41 @@ import ThemeSelectorMobile from './ThemeSelectorMobile';
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
     return (
-        <div className="border-b border-base-content/20">
-            <div className="flex items-center justify-between gap-4 flex-wrap text-base-content py-4 px-6 max-w-7xl mx-auto">
-                <div className="tracking-tight text-2xl">
-                    <span className="font-black italic ital">quick</span>
-                    <span className="font-light">Tools</span>
+        <>
+            {' '}
+            <div className="border-b border-base-content/20">
+                <div className="flex items-center justify-between gap-4 flex-wrap text-base-content py-4 px-6 max-w-7xl mx-auto">
+                    <div className="tracking-tight text-2xl">
+                        <span className="font-black italic ital">quick</span>
+                        <span className="font-light">Tools</span>
+                    </div>
+                    <div className="items-center gap-4 tablet:flex hidden">
+                        <ThemeSelector />
+                        <GitHubLogoIcon />
+                    </div>
+                    <div
+                        className="tablet:hidden block cursor-pointer p-2 d-s"
+                        onClick={() => setIsOpen(!isOpen)}
+                    >
+                        <DotsVerticalIcon />
+                    </div>
                 </div>
-                <div className="items-center gap-4 tablet:flex hidden">
-                    <ThemeSelector />
-                    <GitHubLogoIcon />
+            </div>
+            <div
+                className={`absolute top-14 right-0 w-72 flex flex-col tablet:hidden mx-6 mt-0 d-s ${
+                    isOpen ? '' : 'hidden'
+                }`}
+            >
+                <div className="px-6 py-2">
+                    <div className="py-2">About</div>
+                    <div className="py-2">GitHub</div>
                 </div>
-                <div
-                    className="tablet:hidden block cursor-pointer p-2 d-s"
-                    onClick={() => setIsOpen(!isOpen)}
-                >
-                    <DotsVerticalIcon />
-                </div>
-                <div className={`tablet:hidden flex items-center gap-4`}>
+                <div className="flex gap-4 flex-wrap items-center border-t border-base-content/20 py-4 px-6 bg-base-200">
+                    <div>Select theme</div>
                     <ThemeSelectorMobile />
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
