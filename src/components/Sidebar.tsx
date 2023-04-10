9ed1806318508a33df027a5083fac9423f1e7895
Rich1997 <32636319+Rich1997@users.noreply.github.com>
2023-04-10 23:25:38 +0530

    1 file modified, 1 added

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,17 +1,30 @@
+import {
+    CounterClockwiseClockIcon,
+    CubeIcon,
+    DashboardIcon,
+    StarIcon,
+} from '@radix-ui/react-icons';
+
 const Sidebar = () => {
-    const themes = [
-        { value: 'system', option: 'System' },
-        { value: 'light', option: 'Light' },
-        { value: 'dark', option: 'Dark' },
-        { value: 'neon', option: 'Neon' },
-        { value: 'cupcake', option: 'Cupcake' },
+    const sidebarOptions = [
+        { value: 'dashboard', option: 'Dashboard', icon: <DashboardIcon /> },
+        {
+            value: 'recents',
+            option: 'Recents',
+            icon: <CounterClockwiseClockIcon />,
+        },
+        { value: 'favorites', option: 'Favorites', icon: <StarIcon /> },
+        { value: 'suggested', option: 'Suggested', icon: <CubeIcon /> },
     ];
 
     return (
-        <div className="h-full w-64 border-r border-base-content/20 overflow-auto p-6">
-            {themes.map((options, key) => (
-                <div className="text-base py-2 px-4 rounded hover:bg-primary hover:text-primary-content">
-                    {options.option}
+        <div className="h-full w-48 border-r border-base-content/20 overflow-auto py-6 px-3">
+            {sidebarOptions.map((options, key) => (
+                <div
+                    key={key}
+                    className="flex items-center gap-2 text-sm py-2 px-3 rounded hover:bg-primary hover:text-primary-content"
+                >
+                    {options.icon} {options.option}
                 </div>
             ))}
         </div>
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,5 @@
+const Dashboard = () => {
+    return <div className="flex w-full h-full flex max-w-6xl mx-auto"></div>;
+};
+
+export default Dashboard;
