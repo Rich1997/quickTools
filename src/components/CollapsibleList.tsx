0e71b25c86b7c0b75f0af226223ce975e127e9c1
Rich1997 <32636319+Rich1997@users.noreply.github.com>
2023-04-12 21:38:31 +0530

    Add animation to mobile dialog menu

---
+++ src/components/CollapsibleList.tsx
@@ -0,0 +1,17 @@
+import * as Accordion from '@radix-ui/react-accordion';
+
+const CollapsibleList = () => {
+    return (
+        <div></div>
+        // <Accordion.Root>
+        //     <Accordion.Item>
+        //         <Accordion.Header>
+        //             <Accordion.Trigger />
+        //         </Accordion.Header>
+        //         <Accordion.Content />
+        //     </Accordion.Item>
+        // </Accordion.Root>
+    );
+};
+
+export default CollapsibleList;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,13 +32,13 @@ const Navbar = () => {
                         </div>
                     </Dialog.Trigger>
                     <Dialog.Portal>
-                        <Dialog.Content className="block tablet:hidden overflow-auto data-[state=open]:animate-contentShow fixed top-[80px] left-[50%] max-h-[85vh] w-[90vw] translate-x-[-50%] focus:outline-none bg-base-100 rounded d-b">
+                        <Dialog.Content className="block tablet:hidden overflow-auto data-[state=open]:animate-contentShow fixed min-[0px]:top-[240px] max-[346px]:top-[240px] min-[347px]:top-[222px] left-[50%] max-h-[85vh] w-[90vw] translate-x-[-50%] translate-y-[-50%] focus:outline-none bg-base-100 rounded d-b">
                             <div className="flex flex-col gap-6 p-8">
                                 <div>Docs</div>
                                 <div>About</div>
                                 <div>GitHub</div>
                             </div>
-                            <div className="p-8 flex gap-6 flex-wrap items-center border-t border-base-content/10 bg-base-200">
+                            <div className="p-8 flex gap-6 flex-wrap items-center border-t border-base-content/10">
                                 <div>Select theme</div>
                                 <ThemeSelectorMobile />
                             </div>
