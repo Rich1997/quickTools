4e5b10246e9b48c91f0a5e57c32739be072ba905
Rich1997 <32636319+Rich1997@users.noreply.github.com>
2023-04-17 19:46:32 +0530

    1 file modified

--- src/components/RelatedSpace.tsx
+++ src/components/RelatedSpace.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 const RelatedSpace = (props: { children: ReactNode }) => {
     return (
-        <div className="fixed top-16 bottom-0 right-[max(0px,calc(50%-575px))] w-64 hidden laptop:block h-full w-64 border-l border-base-content/20 bg-base-100 p-6">
+        <div className="fixed top-16 bottom-0 right-[max(0px,calc(50%-575px))] w-64 hidden laptop:block w-64 border-l border-base-content/20 bg-base-100 p-6">
             {props.children}
         </div>
     );
